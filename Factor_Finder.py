55ada4d176d5e1c94b64b6f092a05ebf47fef057
sbernheim4 <sambernheim@gmail.com>
2015-08-19 23:46:33 -0400

    getting the factors of the numerator and the denominator

---
+++ Factor_Finder.py
@@ -0,0 +1,25 @@
+factors = []
+
+from math import sqrt
+from math import ceil
+
+
+def getFactors(origNum):
+
+	for i in range(2,origNum):
+		if origNum%i == 0:
+			factors.append(i)
+	return factors
+
+numerator = int(raw_input("Enter your numerator\n"))
+denominator = int(raw_input("Enter your denominator\n"))
+
+numeratorFactors = getFactors(numerator)
+factors = []
+denominatorFactors = getFactors(denominator)
+
+print "The factors of the numerator are: "
+print numeratorFactors
+print
+print "The factors of the denominator are: "
+print denominatorFactors
